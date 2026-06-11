7ed90ceb3bcf0c4a7336ffbe968a8f9e84cebe6f
alehosainim-afk <alehosaini.m@gmail.com>
2026-06-11 22:25:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -601,7 +601,7 @@ client.on('interactionCreate', async (interaction) => {
         if (isNaN(usdAmount)) return interaction.reply({ content: 'Invalid amount.', ephemeral: true });
         ticket.usdAmount = usdAmount;
         ticket.usdConfirmed = { trader1: false, trader2: false };
-        const usdConfirmEmbed = new EmbedBuilder().setTitle('\u200b').setDescription(`## <a:loading:1507682188228034586> • USD amount set to\n## \`$${usdAmount.toFixed(2)}\`\nPlease confirm the USD amount.`).setColor(0x2b2d31);
+        const usdConfirmEmbed = new EmbedBuilder().setDescription(`## <a:loading:1507682188228034586> • USD amount set to\n## \`$${usdAmount.toFixed(2)}\`\nPlease confirm the USD amount.`).setColor(0x2b2d31);
         const correctBtn = new ButtonBuilder().setCustomId(`confirm_usd_correct_${ticketId}`).setLabel('Correct').setStyle(ButtonStyle.Success).setEmoji({ id: '1514717904350740600', name: 'Animated_Tick', animated: true });
         const incorrectBtn = new ButtonBuilder().setCustomId(`confirm_usd_incorrect_${ticketId}`).setLabel('Incorrect').setStyle(ButtonStyle.Danger).setEmoji({ id: '1514717874281779330', name: 'Animated_Cross', animated: true });
         const row = new ActionRowBuilder().addComponents(correctBtn, incorrectBtn);
