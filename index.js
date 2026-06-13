4e83587d34b4b31d5ea09bdd83e88ab2d1596a2e
alehosainim-afk <alehosaini.m@gmail.com>
2026-06-13 11:31:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -654,7 +654,7 @@ client.on('interactionCreate', async (interaction) => {
             }
           ]
         });
-        const roleEmbed = new EmbedBuilder().setTitle('🛡️ • Select your role').setDescription('"**Sender**" if you are __Sending__ LTC to the bot.\n"**Receiver**" if you are __Receiving__ LTC *later* from the bot.\n\n**Sender**\n...\n**Receiver**\n...').setColor(0x2b2d31);
+        const roleEmbed = new EmbedBuilder().setTitle('<:roleicon:1447022899419418685> • Select your role').setDescription('> • "__**Sender**__" if you are __Sending__ LTC to the bot.\n> • "__**Receiver**__" if you are __Receiving__ LTC *later* from the bot.\n\n**Sender**\n...\n**Receiver**\n...').setColor(0x2b2d31);
         const senderBtn = new ButtonBuilder().setCustomId(`role_sender_${channel.id}`).setLabel('Sender').setStyle(ButtonStyle.Primary);
         const receiverBtn = new ButtonBuilder().setCustomId(`role_receiver_${channel.id}`).setLabel('Receiver').setStyle(ButtonStyle.Primary);
         const resetBtn = new ButtonBuilder().setCustomId(`role_reset_${channel.id}`).setLabel('Reset').setStyle(ButtonStyle.Danger);
